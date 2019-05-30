ddfeaf3e35317e17905cbba1ee01e81111916c1a
zheng_wenlong <wenlong_zheng@nexty-ele.com>
2019-05-30 16:25:17 +0900

    add als2019 source

---
+++ include/navigation_info.h
@@ -0,0 +1,76 @@
+// Copyright 2019 NEDL CO.,LTD
+#pragma once
+
+#include <stdbool.h>
+#include <stdint.h>
+#include <string>
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ *  @brief get or set navigation infomation
+ */
+class NavigationInfo
+{
+private:
+	/*	AllSessions	*/
+	uint32_t mSessionHandle;		//sessionhandle
+	std::string	mClient;			//client name
+
+	/* GetPosition */
+	char mCurrentLatitude[256];		//current latitude info
+	char mCurrentLongitude[256];	//current longitude info
+	char mCurrentHeading[256];	     //current heading info
+
+	/*	getAllroutes	*/
+	uint32_t mRoutes;				//current route count
+
+	/*	getdestination	*/
+	char mDestLatitude[256];		//Destination latitude info
+	char mDestLongitude[256];		//Destination longitude info
+
+	/*	demodestination	*/
+	char mDemoLatitude[256];		//current latitude info when in demo
+	char mDemoLongitude[256];		//current longitude info when in demo
+	char mDemoDistance[256];		//the distance from current position(along the route)
+	char mDemoDirection[256];		//current car direction
+
+public:
+	//get&set function for per members
+	void setNaviInfoSessionHandle( char* sessionHandle );
+	uint32_t getNaviInfoSessionHandle();
+
+	void setNaviInfoClient( std::string client );
+	std::string getNaviInfoClient();
+
+	void setNaviInfoCurrentLatitude( char* latitude );
+	char* getNaviInfoCurrentLatitude();
+
+	void setNaviInfoCurrentLongitude( char* longitude );
+	char* getNaviInfoCurrentLongitude();
+
+	void setNaviInfoCurrentHeading( char* heading );
+	char* getNaviInfoCurrentHeading();
+
+	void setNaviInfoAllRoutes( char* route );
+	uint32_t getNaviInfoAllRoutes();
+
+	void setNaviInfoDestLatitude( char* latitude );
+	char* getNaviInfoDestLatitude();
+
+	void setNaviInfoDestLongitude( char* longitude );
+	char* getNaviInfoDestLongitude();
+
+	void setNaviInfoDemoLatitude( char* latitude );
+	char* getNaviInfoDemoLatitude();
+
+	void setNaviInfoDemoLongitude( char* longitude );
+	char* getNaviInfoDemoLongitude();
+
+	void setNaviInfoDemoDistance( char* distance );
+	char* getNaviInfoDemoDistance();
+
+	void setNaviInfoDemoDirection( char* direction );
+	char* getNaviInfoDemoDirection();
+};
+
---
+++ src/navigation_info.cpp
@@ -0,0 +1,126 @@
+// Copyright 2019 NEDL CO.,LTD
+
+#include<string.h>
+#include "navigation_info.h"
+
+using namespace std;
+
+void NavigationInfo::setNaviInfoSessionHandle( char* sessionHandle )
+{
+	mSessionHandle = stoi(sessionHandle);
+}
+
+uint32_t NavigationInfo::getNaviInfoSessionHandle()
+{
+	return mSessionHandle;
+}
+
+void NavigationInfo::setNaviInfoClient( string client )
+{
+	mClient = client;
+}
+
+string NavigationInfo::getNaviInfoClient()
+{
+	return mClient;
+}
+
+void NavigationInfo::setNaviInfoCurrentLatitude( char* latitude )
+{
+	strcpy(mCurrentLatitude,latitude);
+}
+
+void NavigationInfo::setNaviInfoCurrentHeading( char* heading )
+{
+	strcpy(mCurrentHeading,heading);
+}
+
+char* NavigationInfo::getNaviInfoCurrentLatitude()
+{
+	return mCurrentLatitude;
+}	
+
+void NavigationInfo::setNaviInfoCurrentLongitude( char* longitude )
+{
+	strcpy(mCurrentLongitude,longitude);
+}
+
+char* NavigationInfo::getNaviInfoCurrentLongitude()
+{
+	return mCurrentLongitude;
+}
+
+char* NavigationInfo::getNaviInfoCurrentHeading()
+{
+	return mCurrentHeading;
+}
+
+void NavigationInfo::setNaviInfoAllRoutes( char* route )
+{
+	mRoutes = stoi(route);
+}
+
+uint32_t NavigationInfo::getNaviInfoAllRoutes()
+{
+	return mRoutes;
+}
+
+void NavigationInfo::setNaviInfoDestLatitude( char* latitude )
+{
+	strcpy(mDestLatitude,latitude);
+}
+
+char* NavigationInfo::getNaviInfoDestLatitude()
+{
+	return mDestLatitude;
+}	
+
+void NavigationInfo::setNaviInfoDestLongitude( char* longitude )
+{
+	strcpy(mDestLongitude,longitude);
+}
+
+char* NavigationInfo::getNaviInfoDestLongitude()
+{
+	return mDestLongitude;
+}
+
+void NavigationInfo::setNaviInfoDemoLatitude( char* latitude )
+{
+	strcpy(mDemoLatitude,latitude);
+}
+
+char* NavigationInfo::getNaviInfoDemoLatitude()
+{
+	return mDemoLatitude;
+}	
+
+void NavigationInfo::setNaviInfoDemoLongitude( char* longitude )
+{
+	strcpy(mDemoLongitude,longitude);
+}
+
+char* NavigationInfo::getNaviInfoDemoLongitude()
+{
+	return mDemoLongitude;
+}
+
+void NavigationInfo::setNaviInfoDemoDistance( char* distance )
+{
+	strcpy(mDemoDistance,distance);
+}
+
+char* NavigationInfo::getNaviInfoDemoDistance()
+{
+	return mDemoDistance;
+}	
+
+void NavigationInfo::setNaviInfoDemoDirection( char* direction )
+{
+	strcpy(mDemoDirection,direction);
+}
+
+char* NavigationInfo::getNaviInfoDemoDirection()
+{
+	return mDemoDirection;
+}
